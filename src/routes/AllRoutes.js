03d47089c9dfcf13b1d8b185fead62120b6cdcc6
Colin Beltz <colinbeltz@gmail.com>
2023-08-10 19:59:46 -0500

    Completed Lesson 15, Step 5 beginning steps / Header

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage } from "../pages";
+import { HomePage, ProductList } from "../pages";
 
 export const AllRoutes = () => {
   return (
     <>
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/products" element={<ProductList />} />
         </Routes>
     </>
   )
