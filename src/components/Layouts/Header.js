f1869c52bfa819d74d7b119bcb4bc99579e011c0
Colin Beltz <colinbeltz@gmail.com>
2023-08-16 21:21:40 -0500

    Completed 15-20. Added dropdown menu functionality.

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Search } from "../Sections/Search";
+import { DropdownLoggedOut } from "../index";
 import Logo from "../../assets/logo.png";
 export const Header = () => {
 
     const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMode")) || false);
     const [searchSection, setSearchSection] = useState(false);
+    const [dropdown, setDropdown] = useState(false);
 
     useEffect(() => {
         localStorage.setItem("darkMode", JSON.stringify(darkMode));
@@ -32,7 +34,8 @@ export const Header = () => {
                         <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">0</span>
                     </span>                    
                     </Link>
-                    <span className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 dark:text-white"></span>
+                    <span onClick={() => setDropdown(!dropdown)} className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 dark:text-white"></span>
+                    { dropdown && <DropdownLoggedOut /> }
                 </div>
             </div>
         </nav>
