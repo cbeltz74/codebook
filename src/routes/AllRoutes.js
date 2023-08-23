6a995f244c2ea873fea7cb6e8896f15bbb98630d
Colin Beltz <colinbeltz@gmail.com>
2023-08-22 20:08:27 -0500

    Completed 15-33 Protected Routes

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage, ProductsList, ProductDetail, Login, Register, CartPage } from "../pages";
+import { ProtectedRoutes } from "./ProtectedRoutes";
 
 export const AllRoutes = () => {
+
   return (
     <>
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="products" element={<ProductsList />} />
             <Route path="products/:id" element={<ProductDetail />} />
+            
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
-            <Route path="cart" element={<CartPage />} />
+
+            <Route path="cart" element={<ProtectedRoutes><CartPage /></ProtectedRoutes>} />
         </Routes>
     </>
   )
