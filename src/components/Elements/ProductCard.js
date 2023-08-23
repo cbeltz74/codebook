efd81809046f118e96ecabf24fbaa3bedbc744d2
Colin Beltz <colinbeltz@gmail.com>
2023-08-22 20:59:19 -0500

    Completed 15-34 Checkout Modal name, user values

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -20,9 +20,9 @@ export const ProductCard = ({product}) => {
 
     }, [cartList, product.id]);
 
-    function handleClick(product){
-        addToCart(product);
-    }
+    // function handleClick(product){
+    //     addToCart(product);
+    // }
 
   return (
     <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
